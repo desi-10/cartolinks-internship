bd07da7b8cfe8446681c3490b16cbbc2e262c3fc
Desmond Kudjuh <desmondkudjuh99@gmail.com>
2025-09-27 12:38:43 +0000

    updated carousel card

--- src/components/common/carousel-card.tsx
+++ src/components/common/carousel-card.tsx
@@ -46,7 +46,7 @@ const CarouselCard = ({
             <h3 className="text-2xl font-bold mt-2">{subtitle}</h3>
             <p className="text-sm mt-1 max-w-xl ">{description}</p>
           </div>
-          <button className="mt-4 bg-white text-black px-2 py-2 text-sm rounded-full w-36 dark:bg-gray-800 dark:text-gray-100 dark:hover:bg-gray-700 transition">
+          <button className="mt-4 bg-white text-black px-2 py-2 text-sm rounded-full w-36 dark:bg-gray-800 dark:text-gray-100 dark:hover:bg-gray-700 transition cursor-pointer">
             {buttonText}
           </button>
         </div>
